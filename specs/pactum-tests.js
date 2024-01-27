74dabcfa3bbd36f0a704a28e4bd79c7049157635
Gustavo Cardoso <gusstavocardoso@gmail.com>
2024-01-27 11:22:09 -0300

    refactor: formatting the code

--- specs/pactum-tests.js
+++ specs/pactum-tests.js
@@ -1,75 +1,73 @@
 const { spec, request } = require('pactum');
 const { books } = require('../data/books.json');
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 
 let token;
 let bookingId;
 
-let username = process.env.USER
-let password = process.env.PASS
+let username = process.env.USER;
+let password = process.env.PASS;
 
 request.setBaseUrl(process.env.BASE_URL);
-request.setDefaultHeaders({ 
-    'Content-Type': 'application/json', 
-    'Accept': 'application/json' 
+request.setDefaultHeaders({
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
 });
 
 describe('Testando API restful-booker com PactumJS', () => {
+  before(async () => {
+    await spec()
+      .post('/auth')
+      .withJson({
+        username: username,
+        password: password,
+      })
+      .expectStatus(200)
+      .returns((response) => {
+        token = response.res.body.token;
+        console.log(token);
+      });
+  });
 
-    it('Should generate a token', async () => {
+  it('Should register a book', async () => {
+    await spec()
+      .post('/booking')
+      .withJson(books.create)
+      .expectStatus(200)
+      .returns((response) => {
+        bookingId = response.res.body.bookingid;
+        console.log(bookingId);
+      });
+  });
 
-        await spec()
-            .post('/auth')
-            .withJson({
-                "username": username,
-                "password": password
-            })
-            .expectStatus(200)
-            .returns((response) => {
-                token = response.res.body.token
-                console.log(token);
-            });
-    });
+  it('Shoul list a book by id', async () => {
+    await spec()
+      .get(`/booking/${bookingId}`)
+      .expectStatus(200)
+      .returns((response) => {
+        let createdBook = response.res.body;
+        console.log(createdBook);
+      });
+  });
 
-    it('Should register a book', async () => {
-        await spec()
-            .post('/booking')
-            .withJson(books.create)
-            .expectStatus(200)
-            .returns((response) => {
-                bookingId = response.res.body.bookingid
-                console.log(bookingId);
-            });
-    });
+  it('Should update a book', async () => {
+    await spec()
+      .put(`/booking/${bookingId}`)
+      .withCookies('token', `${token}`)
+      .withJson(books.update)
+      .expectStatus(200)
+      .returns((response) => {
+        let updateBook = response.res.body;
+        console.log(updateBook);
+      });
+  });
 
-    it('Shoul list a book by id', async () => {
-        await spec()
-            .get(`/booking/${bookingId}`)
-            .expectStatus(200)
-            .returns((response) => {
-                let createdBook = response.res.body
-                console.log(createdBook);
-            });
-    });
-
-    it('Should update a book', async () => {
-        await spec()
-            .put(`/booking/${bookingId}`)
-            .withCookies('token', `${token}`)
-            .withJson(books.update)
-            .expectStatus(200)
-            .returns((response) => {
-                let updateBook = response.res.body
-                console.log(updateBook);
-            });
-    });
-
-    it('Should remove a book', async () => {
-        await spec()
-            .delete(`/booking/${bookingId}`)
-            .withCookies('token', `${token}`)
-            .expectStatus(201);
-    });
-})
+  it('Should remove a book', async () => {
+    await spec()
+      .delete(`/booking/${bookingId}`)
+      .withCookies('token', `${token}`)
+      .expectStatus(201);
+  });
+});
